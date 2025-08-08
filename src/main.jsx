2a3e00b725c0d7b79a3283f46e557183953f58b8
wasemabujoseph <35872118+wasemabujoseph@users.noreply.github.com>
2025-08-09 00:54:20 +0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,6 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './styles.css'
-import './i18n'
-import FirebaseProvider from './firebaseInit'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./pages/App";
+import "./styles.css";
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FirebaseProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </FirebaseProvider>
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
